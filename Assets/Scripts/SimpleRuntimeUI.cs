50ab60ec728df8d73dfdb685dee4c6105d0f619d
Artem Karaman <karaman55_09@mail.ru>
2022-09-25 23:21:58 +0300

    add simple scene with ui document and c# script

---
+++ Assets/Scripts/SimpleRuntimeUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SimpleRuntimeUI : MonoBehaviour
+{
+    [SerializeField]
+    private UIDocument _uiDocument;
+    private Button _button;
+    private Toggle _toggle;
+    private int _clickCount;
+
+    void OnEnable()
+    {
+        _button = _uiDocument.rootVisualElement.Q<Button>();
+        _toggle = _uiDocument.rootVisualElement.Q<Toggle>();
+
+        _button.RegisterCallback<ClickEvent>(PrintClickMessage);
+    }
+
+    private void PrintClickMessage(ClickEvent evt)
+    {
+        ++_clickCount;
+        var button = evt.currentTarget as Button;
+        Debug.Log($"{button.name} was clicked!" + (_toggle.value ?
+                     " Count: " + _clickCount : "")
+                );
+    }
+}
